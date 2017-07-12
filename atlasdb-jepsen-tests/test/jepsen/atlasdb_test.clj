7c7db4a1d85c5efa56ba8062cf95cf9aacaa7201
Jeremy Kong <jeremykong@hotmail.com>
2017-07-12 13:01:38 +0100

    [temp] only run async tests so we can get more signal

--- atlasdb-jepsen-tests/test/jepsen/atlasdb_test.clj
+++ atlasdb-jepsen-tests/test/jepsen/atlasdb_test.clj
@@ -9,11 +9,11 @@
 ;; Using Clojure's testing framework, we initiate our test runs
 ;; Tests successful iff the value for the key ":valid?" is truthy
 
-(deftest sync-lock-test-crash
-  (is (:valid? (:results (jepsen/run! (lock/sync-lock-test timelock/crash-nemesis))))))
-
-(deftest sync-lock-test-partition
-  (is (:valid? (:results (jepsen/run! (lock/sync-lock-test (nemesis/partition-random-halves)))))))
+;(deftest sync-lock-test-crash
+;  (is (:valid? (:results (jepsen/run! (lock/sync-lock-test timelock/crash-nemesis))))))
+;
+;(deftest sync-lock-test-partition
+;  (is (:valid? (:results (jepsen/run! (lock/sync-lock-test (nemesis/partition-random-halves)))))))
 
 (deftest async-lock-test-crash
   (is (:valid? (:results (jepsen/run! (lock/async-lock-test timelock/crash-nemesis))))))
@@ -21,8 +21,8 @@
 (deftest async-lock-test-partition
   (is (:valid? (:results (jepsen/run! (lock/async-lock-test (nemesis/partition-random-halves)))))))
 
-(deftest timestamp-test-crash
-  (is (:valid? (:results (jepsen/run! (timestamp/timestamp-test timelock/crash-nemesis))))))
-
-(deftest timestamp-test-partition
-  (is (:valid? (:results (jepsen/run! (timestamp/timestamp-test (nemesis/partition-random-halves)))))))
+;(deftest timestamp-test-crash
+;  (is (:valid? (:results (jepsen/run! (timestamp/timestamp-test timelock/crash-nemesis))))))
+;
+;(deftest timestamp-test-partition
+;  (is (:valid? (:results (jepsen/run! (timestamp/timestamp-test (nemesis/partition-random-halves)))))))
