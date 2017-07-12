c4ccd0ae6a646a1f04b6fcf2bb6abf596178bdea
Jeremy Kong <jeremykong@hotmail.com>
2017-07-12 11:29:37 +0100

    Jepsen Async Lock

--- atlasdb-jepsen-tests/test/jepsen/atlasdb_test.clj
+++ atlasdb-jepsen-tests/test/jepsen/atlasdb_test.clj
@@ -9,12 +9,6 @@
 ;; Using Clojure's testing framework, we initiate our test runs
 ;; Tests successful iff the value for the key ":valid?" is truthy
 
-(deftest timestamp-test-crash
-  (is (:valid? (:results (jepsen/run! (timestamp/timestamp-test timelock/crash-nemesis))))))
-
-(deftest timestamp-test-partition
-  (is (:valid? (:results (jepsen/run! (timestamp/timestamp-test (nemesis/partition-random-halves)))))))
-
 (deftest sync-lock-test-crash
   (is (:valid? (:results (jepsen/run! (lock/sync-lock-test timelock/crash-nemesis))))))
 
@@ -26,3 +20,9 @@
 
 (deftest async-lock-test-partition
   (is (:valid? (:results (jepsen/run! (lock/async-lock-test (nemesis/partition-random-halves)))))))
+
+(deftest timestamp-test-crash
+  (is (:valid? (:results (jepsen/run! (timestamp/timestamp-test timelock/crash-nemesis))))))
+
+(deftest timestamp-test-partition
+  (is (:valid? (:results (jepsen/run! (timestamp/timestamp-test (nemesis/partition-random-halves)))))))
